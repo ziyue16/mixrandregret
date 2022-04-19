83fd02c9f54a1f89316e07eed5034cceb75b37ad
Álvaro A. Gutiérrez Vargas <algutierre@fen.uchile.cl>
2022-04-19 18:11:26 +0200

    apollo benchmark

---
+++ certification/apollo_benckmark/gen_data_for_apollo.do
@@ -0,0 +1,163 @@
+*! 1.1.0 17 Jan 2022
+* Author: Ziyue ZHU (ziyue.zhu@student.kuleuven.be) 
+
+/*==================================*/
+/*====      Setting up        ======*/
+/*==================================*/
+clear all
+/*You need to adjust this and change it for the folder where you will be developing the command.*/
+
+global route = "C:\Users\u0133260\Documents\_local_git_repos\mixrandregret\src"
+cd "$route"
+/*This will search for the adofile of the command.*/
+findfile mixrandregret.ado
+
+
+set seed 777
+local J  3   /*Number of alternatives*/
+local N  500 /*Number of Individuals*/
+local t  6 /*Number of choice sets per individual*/
+
+/*===================================================*/
+/*===  Define the inputs for mata evaluator    ======*/
+/*===================================================*/
+
+/*Star defining the inputs for the mata evaluator defined latter.*/
+local rhs "x1 x2"   // right hand side variables: defined by hand
+local lhs "choice"  // left hand side variables: defined by hand
+local group "id_cs" // variable with choice sets
+local id "id_ind"   // individual ID
+
+local nrep 50       // # of halton draws per random variable
+local kfix 1         // # of fixed variable (0 in this script)
+local krnd 2        // # of random variables (both 2 variables, x1 x2)
+local burn 15       // # of burning draws in halton 
+
+/*Set total number of observations on the data*/
+set obs  `=`N'*`t'*`J''
+* 20 individuals choose from 25 choice sets, and each choice set includes 3 alternatives
+*ssc install seq 
+
+/* id_ind = id for each individual */
+seq id_ind, b(`=`J'*`t'')
+
+/* id_cs_of_ind_n = id for each choice situation for each individual n */
+seq id_cs_of_ind_n,  f(1) t(`=`J'') b(`=`J'')
+
+/* id_cs = id choice situations without considering the individual */
+seq id_cs,  f(1) t(`=`J'*`N'*`t'') b(`=`J'')
+sort id_cs
+
+/* alternative = size of the choice set from where the individual choose*/
+seq alternative, t(`J')
+
+local s = 5
+/*two generic attributes (random)*/
+gen x1 =  runiform(-`s',`s')
+gen x2 =  runiform(-`s',`s')
+
+/*create a fix attribute */
+gen x_fix = rnormal(0,1)
+
+/* parameters for distribution */
+local mu_1  = 1
+local mu_2  = 2
+
+local sigma_1  = 0.5
+local sigma_2  = 0.5
+
+mata: b1 = `mu_1' :+ sort(J(`=`J'*`t'', 1, rnormal(`N',1,0,1)),1) :* `sigma_1'
+mata: b2 = `mu_2' :+ sort(J(`=`J'*`t'', 1, rnormal(`N',1,0,1)),1) :* `sigma_2'
+
+//mata: exp_b1 = exp(b1)
+//mata: exp_b1 = exp(b2)
+
+
+mata: b = b1 , b2
+
+
+global individuals = "id_ind"
+global choice_sets = "id_cs"
+sort id_cs
+
+
+/*===================================================*/
+/*====             DATA Generation             ======*/
+/* Two (independent) normally distributed parameters */
+/*===================================================*/
+
+mata:
+/* program a function to calculate observed regret */
+function RRM_log_sim_data(real matrix x_n, 
+						  real rowvector betas)
+{
+  real scalar i, j
+  real matrix regret_n 
+  
+  regret_n = J(rows(x_n), cols(x_n), 0)
+  for(i=1; i <= rows(x_n); ++i) { 
+	for(j=1; j <= rows(x_n); ++j) { 
+		if (i!=j) { 
+			r_i = ln(1 :+ exp( betas :* ( x_n[j , . ] :-  x_n[i, . ]))) 				
+			regret_n[i, . ] = regret_n[i, . ] :+ r_i 		
+			} 
+		}	  
+	}
+return(regret_n)
+}
+		
+// Generates a view of all attributes (M) x*
+st_view(X = ., ., "x1 x2")
+
+// Generates a view id choice situations
+st_view(panvar_choice_sets = ., ., "id_cs")
+		
+// set up panel information where each panel unit refers to a choice set 
+task_n = panelsetup(panvar_choice_sets, 1)
+
+// # of choice sets 
+npanels_choice_sets = panelstats(task_n)[1]
+
+for(t=1; t<=npanels_choice_sets; t++) {
+
+
+	// read in data rows pertaining to subject n & store in a matrix suffixed _n
+	x_n = panelsubmatrix(X, t, task_n) 
+	b_n = panelsubmatrix(b, t, task_n) 
+	
+	
+	/*Observed (deterministic) Regret*/
+	R_i = rowsum(RRM_log_sim_data(x_n, b_n[1,])) /*take first row of block of parameters*/
+
+    /* Type 1 error */
+	epsilon  = -1*log(-log(runiform(rows(R_i),1,0,1)))
+    /*Random regret */
+	RR_i = R_i  :+ epsilon
+
+    EXP_RR_i = exp(-RR_i)
+
+
+	P_i   = EXP_RR_i :/ quadcolsum(EXP_RR_i, 1)
+
+	choice_i =  (P_i	 :==max(P_i))
+	// collect all choices of choice situations (t) of individual (n)
+	  if (t==1)  choice = choice_i
+	  else       choice = choice   \ choice_i
+
+}
+// Creates a new Stata variable called "choice"    
+ idx = st_addvar("float", "choice")
+// save the content of Y on "choice" Stata variable
+ st_store(., idx, choice)
+end
+
+
+bys id_cs: egen choice_wide = max(choice*alternative)
+
+drop choice 
+
+reshape wide x1 x2 x_fix   , i(id_cs) j(alternative)
+
+export excel using "C:\Users\u0133260\Documents\_local_git_repos\mixrandregret\certification\apollo_benckmark\data.xls", firstrow(variables) nolabel replace
+
+
---
+++ certification/apollo_benckmark/mixrandregret_apollo.R
@@ -0,0 +1,225 @@
+library(haven)
+library(apollo)
+#### LOAD DATA ####
+
+database =as.data.frame( read_excel("data.xls"))
+
+# ################################################################# #
+#### LOAD LIBRARY AND DEFINE CORE SETTINGS                       ####
+# ################################################################# #
+
+### Initialise code
+apollo_initialise()
+
+### Set core controls
+apollo_control = list(
+  modelName       = "MMNL_preference_space",
+  modelDescr      = "Mixed random regret logit model on simulated data",
+  indivID         = "id_ind",  
+  mixing          = TRUE,
+  nCores          = 10,
+  outputDirectory = NULL
+)
+
+
+### Vector of parameters, including any that are kept fixed in estimation
+apollo_beta = c(mu_x1    = 1   ,
+                sd_x1    = 0.25,
+                mu_x2    = 2   ,
+                sd_x2    = 0.25,
+                b_fix  = 0.01  )
+
+### Vector with names (in quotes) of parameters to be kept fixed at their starting value in apollo_beta, use apollo_beta_fixed = c() if none
+apollo_fixed = c()
+
+
+
+
+# ################################################################# #
+#### DEFINE RANDOM COMPONENTS                                    ####
+# ################################################################# #
+
+### Set parameters for generating draws
+apollo_draws = list(
+  interDrawsType = "halton",
+  interNDraws    = 1000,
+  interUnifDraws = c(),
+  interNormDraws = c("draws_x1","draws_x2"),
+  intraDrawsType = "halton",
+  intraNDraws    = 0,
+  intraUnifDraws = c(),
+  intraNormDraws = c()
+)
+
+
+### Create random parameters
+apollo_randCoeff = function(apollo_beta, apollo_inputs){
+  randcoeff = list()
+  
+  randcoeff[["b_x1"]] =  mu_x1 + sd_x1 * draws_x1 
+  randcoeff[["b_x2"]] =  mu_x2 + sd_x2 * draws_x2 
+
+  return(randcoeff)
+}
+
+
+apollo_inputs = apollo_validateInputs()
+
+
+
+# ################################################################# #
+#### DEFINE MODEL AND LIKELIHOOD FUNCTION                        ####
+# ################################################################# #
+
+apollo_probabilities=function(apollo_beta, apollo_inputs, functionality="estimate"){
+  
+  ### Function initialisation: do not change the following three commands
+  ### Attach inputs and detach after function exit
+  apollo_attach(apollo_beta, apollo_inputs)
+  on.exit(apollo_detach(apollo_beta, apollo_inputs))
+  
+  
+  
+  ### x1 differences
+# dx1_1 = ( x11 -  x11 ) 
+  dx1_2_1 = ( x12 -  x11 ) 
+  dx1_3_1 = ( x13 -  x11 ) 
+
+  dx1_1_2 = ( x11 -  x12 ) 
+# dx1_2_2 = ( x12 -  x12 ) 
+  dx1_3_2 = ( x13 -  x12 ) 
+
+  dx1_1_3 = ( x11 -  x13 ) 
+  dx1_2_3 = ( x12 -  x13 ) 
+#  dx1_3_3 = ( x13 -  x13 ) 
+  
+
+  ### x2 differences
+# dx2_1_1 = ( x21 -  x21 ) 
+  dx2_2_1 = ( x22 -  x21 ) 
+  dx2_3_1 = ( x23 -  x21 ) 
+  
+  dx2_1_2 = ( x21 -  x22 ) 
+# dx2_2_2 = ( x22 -  x22 ) 
+  dx2_3_2 = ( x23 -  x22 ) 
+  
+  dx2_1_3 = ( x21 -  x23 ) 
+  dx2_2_3 = ( x22 -  x23 ) 
+# dx2_3_3 = ( x23 -  x23 ) 
+  
+### x_fix  differences
+# dfix_1_1 = ( x_fix1 -  x_fix1 ) 
+  dfix_2_1 = ( x_fix2 -  x_fix1 ) 
+  dfix_3_1 = ( x_fix3 -  x_fix1 ) 
+  
+  dfix_1_2 = ( x_fix1 -  x_fix2 ) 
+# dfix_2_2 = ( x_fix2 -  x_fix2 ) 
+  dfix_3_2 = ( x_fix3 -  x_fix2 ) 
+  
+  dfix_1_3 = ( x_fix1 -  x_fix3 ) 
+  dfix_2_3 = ( x_fix2 -  x_fix3 ) 
+# dfix_3_3 = ( x_fix3 -  x_fix3 ) 
+  
+  
+  
+  ### Create list of probabilities P
+  P = list()
+  
+  ### List of utilities: these must use the same names as in mnl_settings, order is irrelevant
+  V = list()
+  V[["alt1"]] = - log(1+exp(b_x1  * dx1_2_1  ))  - log(1+exp(b_x1 * dx1_3_1 )) + 
+                - log(1+exp(b_x2  * dx2_2_1  ))  - log(1+exp(b_x2 * dx2_3_1 )) +     
+                - log(1+exp(b_fix *dfix_2_1 )) - log(1+exp(b_fix * dfix_3_1 ))  
+    
+  V[["alt2"]] = - log(1+exp(b_x1  * dx1_1_2  ))  - log(1+exp(b_x1 * dx1_3_2 )) + 
+                - log(1+exp(b_x2  * dx2_1_2  ))  - log(1+exp(b_x2 * dx2_3_2 )) +     
+                - log(1+exp(b_fix *dfix_1_2 )) - log(1+exp(b_fix * dfix_3_2 ))  
+  
+  V[["alt3"]] = - log(1+exp(b_x1  * dx1_1_3  ))  - log(1+exp(b_x1 * dx1_2_3 )) + 
+                - log(1+exp(b_x2  * dx2_1_3  ))  - log(1+exp(b_x2 * dx2_2_3 )) +     
+                - log(1+exp(b_fix *dfix_1_3 )) - log(1+exp(b_fix * dfix_2_3 ))  
+  
+  
+  
+  ### Define settings for MNL model component
+  mnl_settings = list(
+    alternatives  = c(alt1=1, alt2=2, alt3=3),
+    avail         = list(alt1=1, alt2=1, alt3=1),
+    choiceVar     = choice_wide,
+    utilities     = V
+  )
+  
+  ### Compute probabilities using MNL model
+  P[["model"]] = apollo_mnl(mnl_settings, functionality)
+  
+  ### Take product across observation for same individual
+  P = apollo_panelProd(P, apollo_inputs, functionality)
+  
+  ### Average across inter-individual draws
+  P = apollo_avgInterDraws(P, apollo_inputs, functionality)
+  
+  ### Prepare and return outputs of function
+  P = apollo_prepareProb(P, apollo_inputs, functionality)
+  return(P)
+}
+
+
+
+
+model = apollo_estimate(apollo_beta, apollo_fixed,apollo_probabilities, apollo_inputs)
+
+# ################################################################# #
+#### MODEL OUTPUTS                                               ####
+# ################################################################# #
+
+# ----------------------------------------------------------------- #
+#---- FORMATTED OUTPUT (TO SCREEN)                               ----
+# ----------------------------------------------------------------- #
+# apollo_modelOutput(model)
+# 
+# 
+# Model run by u0133260 using Apollo 0.2.7 on R 4.1.2 for Windows.
+# www.ApolloChoiceModelling.com
+# 
+# Model name                       : MMNL_preference_space
+# Model description                : Mixed random regret logit model on simulated data
+# Model run at                     : 2022-04-19 17:58:14
+# Estimation method                : bfgs
+# Model diagnosis                  : successful convergence 
+# Number of individuals            : 500
+# Number of rows in database       : 3000
+# Number of modelled outcomes      : 3000
+# 
+# Number of cores used             :  10 
+# Number of inter-individual draws : 1000 (halton)
+# 
+# LL(start)                        : -744.14
+# LL(0)                            : -3295.84
+# LL(C)                            : -3293.8
+# LL(final)                        : -714.27
+# Rho-square (0)                   :  0.7833 
+# Adj.Rho-square (0)               :  0.7818 
+# Rho-square (C)                   :  0.7831 
+# Adj.Rho-square (C)               :  0.7816 
+# AIC                              :  1438.54 
+# BIC                              :  1468.57 
+# 
+# Estimated parameters             :  5
+# Time taken (hh:mm:ss)            :  00:03:15.84 
+# pre-estimation              :  00:01:8.21 
+# estimation                  :  00:00:49.28 
+# post-estimation             :  00:01:18.35 
+# Iterations                       :  15  
+# Min abs eigenvalue of Hessian    :  76.59837 
+# 
+# Unconstrained optimisation.
+# 
+# Estimates:
+#   Estimate        s.e.   t.rat.(0)    Rob.s.e. Rob.t.rat.(0)
+# mu_x1    0.885367     0.05194     17.0473     0.05477       16.1655
+# sd_x1    0.359805     0.03572     10.0725     0.04293        8.3811
+# mu_x2    1.808286     0.09631     18.7764     0.09757       18.5327
+# sd_x2   -0.097858     0.08669     -1.1288     0.11758       -0.8323
+# b_fix    0.008325     0.04017      0.2072     0.04067        0.2047
+
+
